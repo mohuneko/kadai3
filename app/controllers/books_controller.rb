a806e56be5297241a2d213ec18a307c553ea0198
mohuneko <b0wdmohu@gmail.com>
2020-05-03 06:12:47 +0900

    課題2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,81 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :corrent_user, only: [:edit, :update]
+
+  def show
+    book = Book.new
+  	@book = Book.find(params[:id])
+    @user = User.find_by(id: @book.user_id)
+  end
+
+  def index
+    @book = Book.new #投稿フォーム
+  	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+    @user = current_user
+  end
+
+  def new
+  end
+
+  def create
+  	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+    @book.user_id = current_user.id
+  	if @book.save #入力されたデータをdbに保存する。
+      flash[:message] = "successfully"
+  		redirect_to book_path(@book.id), notice: "successfully created book!"#保存された場合の移動先を指定。
+  	else
+      @user = current_user
+  		@books = Book.all
+  		render action: :index
+  	end
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy #削除
+    redirect_to books_path #booksへリダイレクト
+  end
+
+  def corrent_user
+    @book = Book.find(params[:id])
+   if @book.user != current_user
+    redirect_to books_path
+  end
+end
+
+
+  def update
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+      flash[:message] = "successfully"
+  		redirect_to book_path
+      # notice: "successfully updated book!"
+  	else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
+  		render "edit"
+  	end
+  end
+
+  def delete
+  	@book = Book.find(params[:id])
+  	@book.destoy
+  	redirect_to books_path, notice: "successfully delete book!"
+  end
+
+  def corrent_user
+    @book = Book.find(params[:id])
+   if @book.user != current_user
+    redirect_to books_path
+  end
+end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:title, :body, :user_id)
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+	before_action :baria_user, only: [:update, :edit]
+
+  def show
+  	@user = User.find(params[:id])
+  	@books = @user.books
+  	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
+  end
+
+  def new
+    @user = User.new #新規のユーザーを作成している
+  end
+
+  def index
+  	@users = User.all #一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
+  	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
+    @user = current_user
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    flash[:notice] = "Welcome! You have signed up successfully."
+  end
+
+
+  def update
+  	@user = User.find(params[:id]) 
+    @user.update(user_params)
+  if @user.save
+  		redirect_to users_path(@user.id), notice: "successfully updated user!"
+  	else
+  		render "show"
+  	end
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  #url直接防止　メソッドを自己定義してbefore_actionで発動。
+   def baria_user
+  	unless params[:id].to_i == current_user.id
+  		redirect_to user_path(current_user)
+    end
+  end
+end
